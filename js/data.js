fbbfd8117c8c283209b3438f9e0a40c7612dbc2a
Penguinboy <jonathanlsamonte@gmail.com>
2018-06-26 17:11:29 -0700

    add google map error handling

---
+++ js/data.js
@@ -0,0 +1,45 @@
+/* Model Data */
+
+var locations = [{
+        title: 'eSports Arena Oakland',
+        location: {
+            lat: 37.794096,
+            lng: -122.272522
+        }
+    },
+    {
+        title: 'Cloverleaf Family Bowl',
+        location: {
+            lat: 37.535377,
+            lng: -121.965708
+        }
+    },
+    {
+        title: 'Lake Elizabeth',
+        location: {
+            lat: 37.54902,
+            lng: -121.962953
+        }
+    },
+    {
+        title: 'De Anza College',
+        location: {
+            lat: 37.320196,
+            lng: -122.046915
+        }
+    },
+    {
+        title: 'Dave & Busters',
+        location: {
+            lat: 37.418428,
+            lng: -121.897895
+        }
+    },
+    {
+        title: 'San Francisco International Airport (SFO)',
+        location: {
+            lat: 37.621313,
+            lng: -122.378955
+        }
+    }
+];
